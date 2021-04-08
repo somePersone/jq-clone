0fca4a4f00fbfec524b3f5a95c7ceb967afe770a
Eliott Afiat <e.e.afriat@student.tudelft.nl>
2021-04-08 12:34:12 +0200

    read unicode

--- src/Jq/SParser.hs
+++ src/Jq/SParser.hs
@@ -1,14 +1,52 @@
 module Jq.SParser where
 
-import Parsing.Parsing ( Parser, char, item, Alternative((<|>)) )
+import Parsing.Parsing 
 
 parseString :: Parser String
 parseString = do _ <- char '\"'
                  pString
 
 pString :: Parser String
-pString = escape <|> closeString <|> basicChar 
+pString = unicode <|> escape <|> closeString <|> basicChar 
                  
+unicode :: Parser String
+unicode = do _ <- string "\\u"
+	     v <- pure hexToUnicode <*> alphanum <*> alphanum <*> alphanum <*> alphanum
+	     return(v: [])
+	     
+
+digitToInt :: Char -> Int
+digitToInt '0' = 0
+digitToInt '1' = 1
+digitToInt '2' = 2
+digitToInt '3' = 3
+digitToInt '4' = 4
+digitToInt '5' = 5
+digitToInt '6' = 6
+digitToInt '7' = 7
+digitToInt '8' = 8
+digitToInt '9' = 9
+digitToInt 'A' = 10
+digitToInt 'a' = 10
+digitToInt 'B' = 11
+digitToInt 'b' = 11
+digitToInt 'C' = 12
+digitToInt 'c' = 12
+digitToInt 'D' = 13
+digitToInt 'd' = 13
+digitToInt 'E' = 14
+digitToInt 'e' = 14
+digitToInt 'F' = 15
+digitToInt 'f' = 15
+digitToInt 'G' = 16
+digitToInt 'g' = 16
+
+hexToInt :: Char -> Char -> Char -> Char -> Int
+hexToInt a b c d = (digitToInt a * 16^3) + (digitToInt b * 16^2) + (digitToInt c * 16) + digitToInt d
+
+hexToUnicode :: Char -> Char -> Char -> Char -> Char
+hexToUnicode = (((toEnum .) .) .) .  hexToInt 
+
 escape :: Parser String 
 escape = do _ <- char '\\'
             x <- item 
